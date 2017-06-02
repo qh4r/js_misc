8dd8ece29a94b9b6158e11cff0030a0553469044
qh4r <qhr666@gmail.com>
2017-06-03 01:19:51 +0200

    Create tagged_strings.js

---
+++ tagged_strings.js
@@ -0,0 +1,8 @@
+let test = (x,y) => console.log(x,y)
+
+test3 `asd asd ${12} haha ${143}`
+// powyzsze wypisze
+
+// ["asd asd ", " haha ", "" ], 12, 143 
+// 1 argument to lista stringow oddzielonuch ${} a nastepnie wartosci w klamrach
+// tak działa relay
