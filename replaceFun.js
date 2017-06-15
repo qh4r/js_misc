1dac8f644bcf82fa84a177aa3a7bfc5ab3972c7a
qh4r <qhr666@gmail.com>
2017-06-16 00:07:23 +0200

    Added another replace useful behavior example

--- replaceFun.js
+++ replaceFun.js
@@ -2,3 +2,8 @@ let upcaseFirst = strings => strings.join().replace(/\b\w/g, match => match.toUp
 // warto zwrocic uwage na funkcyjne match!
 
 upcaseFirst `krol karol kupil krolowej karolinie` // drukuje : "Krol Karol Kupil Krolowej Karolinie"
+
+// replace jakie dalsze argumenty callbacku przekazuje grupy przechwytujace
+let link2 = '/topics/asfasf/test'
+link2.replace(/(\/topics\/)([^\/]+)(.*)/, (x, ...parts) => `${parts[0]}ZMIANA${parts[2]}`)
+//wynik - /topics/ZMIANA/test
